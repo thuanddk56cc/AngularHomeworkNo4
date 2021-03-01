7c64cdabc97e3f476ac9b7e7d8e9964cd233c296
thuandd <thuandd@biplus.com.vn>
2021-03-01 23:30:43 +0700

    Bai tap ve nha

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,27 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'HomeworkNo4';
+  a: number;
+  b: number;
+  cactoantu = [{id: 1, name:"+"}, {id: 2, name:"-"}, {id: 3, name:"*"}, {id: 4, name:"/"}];
+  toantu = 1;
+  result: number;
+  error: string;
+
+  calc() {
+    this.error = "";
+    if (this.toantu == 1) {
+      this.result = this.a + this.b;
+    } else if (this.toantu == 2) {
+      this.result = this.a - this.b;
+    } else if (this.toantu == 3) {
+      this.result = this.a * this.b;
+    } else if (this.toantu == 4) {
+      if (this.b != 0) {
+        this.result = this.a / this.b;
+      } else {
+        this.error = "Không thể chia cho 0";
+      }
+    }
+  }
 }
